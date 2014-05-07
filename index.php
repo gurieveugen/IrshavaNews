e4429e1ec83e5b127f92366e1d5f5e18a546313a
gurieveugen <gurievinvest@gmail.com>
2014-05-07 15:29:37 +0300

    1

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Irshava News</title>
+	<link rel="stylesheet" href="style.css">
+	<link href="css/jquery.formstyler.css" rel="stylesheet" />
+	<link href="css/flexslider.css" rel="stylesheet" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="js/jquery.formstyler.js"></script>
+	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<div class="wrap">
+		<header class="main">
+			<div class="banner">
+				<img src="http://placehold.it/720x90/ffdf43/666666" alt="Banner">
+			</div>
+			<!-- /.banner -->
+			<div class="link-separator">
+				<a href="#">НОВИНИ ІРШАВИ ТА ІРШАВЩИНИ</a>
+			</div>
+			<!-- /.link-separator -->
+			<div class="tool-box">
+				<ul class="left">
+					<li><img src="images/icon_weather.png" alt="Weather icon"></li>
+					<li>Іршава, 5°</li>
+					<li class="separator"></li>
+					<li>
+						<select name="currency" id="currency">
+							<option value="USD">USD</option>
+							<option value="RUB">RUB</option>
+							<option value="UAH">UAH</option>
+							<option value="EUR">EUR</option>
+						</select>
+					</li>
+					<li><img src="images/icon_arrow_down.png" alt="Arrow down icon"> 8,7243</li>
+				</ul>
+				<!-- /.left -->
+				<ul class="right">
+					<li><a href="#">Вхід</a></li>
+					<li><a href="#">Реєстрація</a></li>
+					<li class="separator"></li>
+					<li><a href="#" class="icon icon-mail"></a></li>
+					<li><a href="#" class="icon icon-rss"></a></li>
+				</ul>
+				<!-- /.right -->
+			</div>
+			<!-- /.tool-box -->
+			<div class="logo">
+				<a href="/"><img src="images/logo.png" alt="Irshava news"></a>
+			</div>
+			<div class="tool-box">
+				<ul class="center">
+					<li>21:30</li>
+					<li class="separator"></li>
+					<li>Понеділок, 3 лютого 2014</li>
+					<li class="separator"></li>
+					<li><a href="#" class="icon icon-vk"></a></li>
+					<li><a href="#" class="icon icon-fb"></a></li>
+					<li><a href="#" class="icon icon-twitter"></a></li>
+				</ul>
+				<form action="#" class="right-search">
+					<input type="text" class="search" placeholder="Шукати">
+					<button type="submit" class="icon-search"></button>
+				</form>
+			</div>
+			<ul class="menu">
+				<li><a href="#" class="active">Головна</a></li>
+				<li><a href="#">Новини</a></li>
+				<li><a href="#">Статті</a></li>
+				<li><a href="#">Фото</a></li>
+				<li><a href="#">Відео</a></li>
+				<li><a href="#">Блоги</a></li>
+			</ul>
+			<!-- /.menu -->
+			<ul class="filter">
+				<li><span>Спецтеми:</span></li>
+				<li><a href="#">Євромайдан</a></li>
+				<li><a href="#">Земельний дерибан</a></li>
+				<li><a href="#">Боржавська вузькоколійка</a></li>
+			</ul>
+			<!-- /.filter -->
+		</header>
+		<section class="main">
+			<div class="content">
+				<article class="featured">
+					<div class="thumb">
+						<img src="images/test4.jpg" alt="Thumb">
+					</div>
+					<div class="text">
+						<h1>В Іршаві Євромайдан пікетував районну раду</h1>
+						<ul class="info">
+							<li>29.01.2014</li>
+							<li><img src="images/icon_views.png" alt="Views"> 1022</li>
+							<li><img src="images/icon_comments.png" alt="Comments"> 4</li>
+						</ul>
+						29 січня під час народного віче в м. Іршава, мітингарі увійшли в будинок РДА та райради. Вони висловили свою незгоду із владою і вимагали негайного зібрання сесії районної ради. також повідомляється, що якісь невідомі намагалися вирвати двері до кабінету голови районної ради, але  їх зупинили самі ж протестувальники. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a> 
+					</div>
+				</article>
+			</div>
+			<div class="sidebar">
+				<div class="block-slider">
+					<div class="title">
+						<h1>Блог</h1>
+						<div class="controls">
+							<a href="#" class="prev-slide icon-arrow-left"></a>
+							<span class="flexslider-status">1/6</span>
+							<a href="#" class="next-slide icon-arrow-right"></a>
+						</div>						
+					</div>
+					<div class="flexslider" data-statusclass="6">
+						<ul class="slides">
+							<li>								
+								<img src="images/test1.jpg" alt="test" class="pull-left">
+								<span class="fio">Василь Кузан:</span>
+								<h1>Народження фестивалю імені Ерделі</h1>
+								<span class="text">
+									Одні фестивалі народжуються, інші вмирають. Як і люди. Це процес. Нормальний процес. Хоча важко смерть назвати нормальним процесом. Вірніше - це еволюція. Певно, таке означення підходить тут найбільше. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a> 
+								</span>
+								<a href="#" class="icon-double-arrow-right"></a>							
+							</li>
+							<li>
+								<img src="images/test1.jpg" alt="test" class="pull-left">
+								<span class="fio">Василь Кузан:</span>
+								<h1>Народження фестивалю імені Ерделі</h1>
+								<span class="text">
+									Одні фестивалі народжуються, інші вмирають. Як і люди. Це процес. Нормальний процес. Хоча важко смерть назвати нормальним процесом. Вірніше - це еволюція. Певно, таке означення підходить тут найбільше. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a>
+								</span>
+								<a href="#" class="icon-double-arrow-right"></a>
+							</li>
+							<li>
+								<img src="images/test1.jpg" alt="test" class="pull-left">
+								<span class="fio">Василь Кузан:</span>
+								<h1>Народження фестивалю імені Ерделі</h1>
+								<span class="text">
+									Одні фестивалі народжуються, інші вмирають. Як і люди. Це процес. Нормальний процес. Хоча важко смерть назвати нормальним процесом. Вірніше - це еволюція. Певно, таке означення підходить тут найбільше. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a>
+								</span>
+								<a href="#" class="icon-double-arrow-right"></a>
+							</li>
+							<li>
+								<img src="images/test1.jpg" alt="test" class="pull-left">
+								<span class="fio">Василь Кузан:</span>
+								<h1>Народження фестивалю імені Ерделі</h1>
+								<span class="text">
+									Одні фестивалі народжуються, інші вмирають. Як і люди. Це процес. Нормальний процес. Хоча важко смерть назвати нормальним процесом. Вірніше - це еволюція. Певно, таке означення підходить тут найбільше. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a>
+								</span>
+								<a href="#" class="icon-double-arrow-right"></a>
+							</li>
+							<li>
+								<img src="images/test1.jpg" alt="test" class="pull-left">
+								<span class="fio">Василь Кузан:</span>
+								<h1>Народження фестивалю імені Ерделі</h1>
+								<span class="text">
+									Одні фестивалі народжуються, інші вмирають. Як і люди. Це процес. Нормальний процес. Хоча важко смерть назвати нормальним процесом. Вірніше - це еволюція. Певно, таке означення підходить тут найбільше. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a>
+								</span>
+								<a href="#" class="icon-double-arrow-right"></a>
+							</li>
+							<li>
+								<img src="images/test1.jpg" alt="test" class="pull-left">
+								<span class="fio">Василь Кузан:</span>
+								<h1>Народження фестивалю імені Ерделі</h1>
+								<span class="text">
+									Одні фестивалі народжуються, інші вмирають. Як і люди. Це процес. Нормальний процес. Хоча важко смерть назвати нормальним процесом. Вірніше - це еволюція. Певно, таке означення підходить тут найбільше. <a href="#"><img src="images/icon_double_arrow_right.png" alt="More"></a>
+								</span>
+								<a href="#" class="icon-double-arrow-right"></a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.flexslider -->
+				</div>
+				<!-- /.block-slider -->
+
+				<div class="block block-banner">
+					<img src="http://placehold.it/310x310/ffdf43/666666" alt="Banner">
+				</div>
+				<!-- /.block-banner -->
+				
+				<div class="block block-slider">
+					<div class="title">
+						<h1>ФОТОРЕПОРТАЖІ</h1>
+						<div class="controls">
+							<a href="#" class="prev-slide icon-arrow-left"></a>
+							<span class="flexslider-status">1/5</span>
+							<a href="#" class="next-slide icon-arrow-right"></a>
+						</div>						
+					</div>
+					<div class="flexslider" data-statusclass="6">
+						<ul class="slides">
+							<li>
+								<h1 class="pull-left">Парад наречених </h1>
+								<span class="fio pull-right">29 фото</span>
+								<img src="images/test2.jpg" alt="test" class="w100p">	
+							</li>
+							<li>
+								<h1 class="pull-left">Парад наречених </h1>
+								<span class="fio pull-right">29 фото</span>
+								<img src="images/test2.jpg" alt="test" class="w100p">	
+							</li>
+							<li>
+								<h1 class="pull-left">Парад наречених </h1>
+								<span class="fio pull-right">29 фото</span>
+								<img src="images/test2.jpg" alt="test" class="w100p">	
+							</li>
+							<li>
+								<h1 class="pull-left">Парад наречених </h1>
+								<span class="fio pull-right">29 фото</span>
+								<img src="images/test2.jpg" alt="test" class="w100p">	
+							</li>
+							<li>
+								<h1 class="pull-left">Парад наречених </h1>
+								<span class="fio pull-right">29 фото</span>
+								<img src="images/test2.jpg" alt="test" class="w100p">	
+							</li>							
+						</ul>
+					</div>
+					<!-- /.flexslider -->
+				</div>
+				<!-- /.block-slider -->
+				
+				<div class="block blcok-tabs">				
+					<ul class="links">
+						<li><a href="#" data-id="#fb-tab" class="active">FACEBOOK</a></li>
+						<li><a href="#" data-id="#vk-tab">ВКОНТАКТЕ</a></li>
+					</ul>
+					<ul class="tabs">
+						<li id="fb-tab" class="show"><img src="images/test3.jpg" alt="Social"></li>
+						<li id="vk-tab"><img src="images/test3.jpg" alt="Social"></li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<footer class="main">
+			
+		</footer>
+	</div>
+</body>
+</html>
